1a128a1e0f3f9ef8e9cf99d98f75efe002c94b27
TangXingmeng <zhudms@163.com>
2016-10-19 14:31:08 +0800

    add
    add

--- 快捷键集锦.java
+++ 快捷键集锦.java
@@ -1,6 +1,26 @@
-﻿ctrl+u  跳转到父类
+﻿
 shift + F6 重命名 
 ctrl +shift +U 切换大小写
 ctrl+alt+o 自动倒包
 
 ctrl+alt+s  打开settings
+
+ctrl+alt+shift +上下方向键 光标向上下移动一个代码段--不怎么好用
+
+Ctrl＋N，需要输入文件名，可以快速打开类
+
+
+debug shift+F9
+run shift+F10
+去掉无效倒包  ctrl +alt+O
+回到刚才编辑的地方 ctrl +shift+back
+移动一行代码 alt+shift+上下
+快速移动一行代码（越过代码块）ctrl+shift+上下
+
+移动屏幕 ctrl+方向键
+
+
+查看：
+Ctrl+Q 显示注释文档
+Ctrl+H 显示类结构图 -->当前类的继承关系
+ctrl+u  跳转到父类
