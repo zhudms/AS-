f4920ac375d5a64de9f07975ac656cc3130562d8
TangXingmeng <tangxm13@163.com>
2017-03-04 16:38:55 +0800

    Update 快捷键集锦.java

--- 快捷键集锦.java
+++ 快捷键集锦.java
@@ -1,24 +1,18 @@
 ﻿
-shift + F6 重命名 
-ctrl +shift +U 切换大小写
-ctrl+alt+o 自动倒包
-
+ 环境 
 ctrl+alt+s  打开settings
+移动屏幕 ctrl+方向键
 
-ctrl+alt+shift +上下方向键 光标向上下移动一个代码段--不怎么好用
-
+ 查找 
 Ctrl＋N，需要输入文件名，可以快速打开类
 Ctrl+shift+f 全局查找某字符串（文件内查找）
 ctrl+F10 自动生成方法注释
 
+ 运行 
 debug shift+F9
 run shift+F10
-去掉无效倒包  ctrl +alt+O
-回到刚才编辑的地方 ctrl +shift+back
-移动一行代码 alt+shift+上下
-快速移动一行代码（越过代码块）ctrl+shift+上下
 
-移动屏幕 ctrl+方向键
+
 
 
 查看：
@@ -27,3 +21,17 @@ Ctrl+H 显示类结构图 -->当前类的继承关系
 ctrl+u  跳转到父类
 ctrl+alt+home 直接眺到当前类引用的布局
 ctrl+鼠标左键悬停 查看声明
+
+ 编辑 
+ shift + F6 重命名 
+ctrl +shift +U 切换大小写
+ctrl+alt+o 自动倒包
+ 去掉无效倒包  ctrl +alt+O
+回到刚才编辑的地方 ctrl +shift+back
+移动一行代码 alt+shift+上下
+快速移动一行代码（越过代码块）ctrl+shift+上下
+ alt+j(ctrl+G,mac) 多行编辑
+ 
+ctrl+alt+shift +上下方向键 光标向上下移动一个代码段--不怎么好用
+
+ 
