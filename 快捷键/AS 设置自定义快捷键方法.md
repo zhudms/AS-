8688bfbf1ed9831e79209fd60f6686b511478df1
Tang <zhudms@163.com>
2019-09-03 17:30:08 +0800

    change

---
+++ 快捷键/AS 设置自定义快捷键方法.md
@@ -0,0 +1,4 @@
+#AndroidStudio中方法注释模板快捷键的设置
+1. 选择Preferences...
+2. 在搜索框输入comment，在Keymap页面选择要设置的快捷键
+
---
+++ 快捷键/win私人版快捷键集锦.java
@@ -0,0 +1,56 @@
+
+ 环境 
+ctrl+alt+s  打开settings
+移动屏幕 ctrl+方向键
+
+ 查找 
+Ctrl＋N，需要输入文件名，可以快速打开类
+Ctrl+shift+f 全局查找某字符串（文件内查找）
+ctrl+F10 自动生成方法注释
+
+ 运行 
+debug shift+F9
+run shift+F10
+
+
+
+
+查看：
+Ctrl+Q 显示注释文档
+Ctrl+H 显示类结构图 -->当前类的继承关系
+ctrl+u  跳转到父类
+ctrl+alt+home 直接眺到当前类引用的布局
+ctrl+鼠标左键悬停 查看声明
+ctrl+shift+N  直接跳转到某文件
+ctrl+E  最近文件（列表）
+Alt+Home 选中文件打开目录级别那里（Navigation Bar）
+Alt+1 快速打开/关闭 文件列表栏
+double shift search every 貌似是搜索文件名
+
+
+ 编辑 
+ shift + F6 重命名 (MAC  control+fn+F6)
+ctrl +shift +U 切换大小写
+ctrl+alt+o 自动倒包
+ 去掉无效倒包  ctrl +alt+O
+回到刚才编辑的地方 ctrl +shift+back
+移动一行代码 alt+shift+上下
+快速移动一行代码（越过代码块）ctrl+shift+上下
+ alt+j(ctrl+G,mac) 多行编辑
+
+
+ctrl+alt+shift +上下方向键 光标向上下移动一个代码段--不怎么好用
+
+生成：
+alt+insert 生成构造方法、get set等方法
+
+AndroidStudio自定义类创建时自动生成的头部注释
+https://www.jianshu.com/p/4c42f4152d84
+https://blog.csdn.net/panhouye/article/details/77528918
+
+自动生成方法注释
+在setting-plugin-Browse repositories 中添加javadoc插件
+
+
+
+ 
